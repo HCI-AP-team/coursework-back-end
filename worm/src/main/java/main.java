e0b3095ccc8b7201ffda3d88b85c1940552babc6
shituweian <42890577+shituweian@users.noreply.github.com>
2020-03-19 12:06:58 +0800

    完善的爬虫数据和输出样例

--- worm/src/main/java/main.java
+++ worm/src/main/java/main.java
@@ -62,10 +62,55 @@ public class main {
         Matcher foreignMatcher=foreignCountry.matcher(result);
 
 
-        //System.out.println(result);
+        System.out.println("以下为世界疫情汇总:\n");
 
 
         while(foreignMatcher.find()){
+
+            String Foreign=foreignMatcher.group(0);
+
+            Foreign=Foreign.substring(Foreign.indexOf("continents"));
+
+            String Continent=Foreign.substring(11,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf("provinceName"));
+
+            String CountryName=Foreign.substring(13,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf("currentConfirmedCount"));
+
+            String CountryTodayConfirmed=Foreign.substring(22,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf("confirmedCount"));
+
+            String CountryTotally=Foreign.substring(15,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf("suspectedCount"));
+
+            String CountrySuspect=Foreign.substring(15,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf(',')+1);
+
+            String CountryCure=Foreign.substring(11,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf(',')+1);
+
+            String CountryDead=Foreign.substring(10,Foreign.indexOf(','));
+
+            Foreign=Foreign.substring(Foreign.indexOf("countryShortCode"));
+
+            String CountryCode=Foreign.substring(17,Foreign.indexOf(','));
+
+            //System.out.println(Foreign);
+
+            Foreign=Foreign.substring(Foreign.indexOf("countryFullName"));
+
+            String CountryFullName=Foreign.substring(16,Foreign.indexOf(','));
+
+
+            System.out.println("所属大陆:"+Continent+" \t国家名称:"+CountryName+" \t今日感染:"+CountryTodayConfirmed+" \t总共感染:"+CountryTotally+" \t疑似病例:"+CountrySuspect+" \t治愈病列:"+CountryCure
+            +" \t死亡病例:"+CountryDead+" \t城市代码:"+CountryCode+" \t城市全称:"+CountryFullName);
+
             //System.out.println(foreignMatcher.group(0));
         }
 
@@ -81,6 +126,10 @@ public class main {
 
         ChinaMatcher.find();                            //此行的功能为为后面过滤掉非中国的地区
 
+        ChinaMatcher.find();
+
+
+        System.out.println("\n\n以下为国内各省份城市疫情汇总:\n");
 
         while(ChinaMatcher.find()){
 
@@ -92,9 +141,44 @@ public class main {
 
             Matcher ProvinceNameMatcher=Province.matcher(ProvinceSituation);
 
-            String ProvinceName=ProvinceSituation.substring(0,ProvinceSituation.indexOf(','));
+            String ProvinceName=ProvinceSituation.substring(13,ProvinceSituation.indexOf(','));
 
-            System.out.println(ProvinceName);
+            try {
+
+                String ProvinceTotally = ProvinceSituation.substring(0, ProvinceSituation.indexOf("comment"));
+
+                //System.out.println(ProvinceTotally);
+
+                ProvinceTotally=ProvinceTotally.substring(ProvinceTotally.indexOf("currentConfirmedCount"));
+
+                //System.out.println(ProvinceTotally);
+
+                String ProvinceTodayConfirmed=ProvinceTotally.substring(22,ProvinceTotally.indexOf(','));
+
+                ProvinceTotally=ProvinceTotally.substring(ProvinceTotally.indexOf(',')+1);
+
+                String ProvinceTotallyConfirmed=ProvinceTotally.substring(15,ProvinceTotally.indexOf(','));
+
+                ProvinceTotally=ProvinceTotally.substring(ProvinceTotally.indexOf(',')+1);
+
+                String ProvinceSuspectCount=ProvinceTotally.substring(15,ProvinceTotally.indexOf(','));
+
+                ProvinceTotally=ProvinceTotally.substring(ProvinceTotally.indexOf(',')+1);
+
+                String ProvinceCured=ProvinceTotally.substring(11,ProvinceTotally.indexOf(','));
+
+                ProvinceTotally=ProvinceTotally.substring(ProvinceTotally.indexOf(',')+1);
+
+                String ProvinceDead=ProvinceTotally.substring(10,ProvinceTotally.indexOf(','));
+
+                System.out.println("省份:"+ProvinceName+"\t 今日确诊:"+ProvinceTodayConfirmed+"\t 总共确诊:"
+                        +ProvinceTotallyConfirmed+"\t 疑似病例:"+ProvinceSuspectCount+"\t 治愈病列:"+ProvinceCured+
+                        "\t 死亡病例:"+ProvinceDead);
+
+            }catch(Exception e){
+                System.out.println();
+            };
+            //System.out.println(ProvinceName);
 
 
             Pattern CitySituation=Pattern.compile("cityName(.*?)}");
@@ -102,16 +186,47 @@ public class main {
             Matcher CitySituationMatcher=CitySituation.matcher(ProvinceSituation);
 
 
-            if(CitySituationMatcher.find()) {
                 while (CitySituationMatcher.find()) {
-                    System.out.println(CitySituationMatcher.group(0));
+
+                    String CityCondition=CitySituationMatcher.group(0);
+
+                    String CityName=CityCondition.substring(9,CityCondition.indexOf(','));
+
+                    CityCondition=CityCondition.substring(CityCondition.indexOf(',')+1);
+
+                    //System.out.println(CityCondition);
+
+                    String TodayConfirmed=CityCondition.substring(22,CityCondition.indexOf(','));
+
+                    CityCondition=CityCondition.substring(CityCondition.indexOf(',')+1);
+
+                    String TotallyConfirmed=CityCondition.substring(15,CityCondition.indexOf(','));
+
+                    CityCondition=CityCondition.substring(CityCondition.indexOf(',')+1);
+
+                    String suspectedCount=CityCondition.substring(15,CityCondition.indexOf(','));
+
+                    CityCondition=CityCondition.substring(CityCondition.indexOf(',')+1);
+
+                    String CuredCount=CityCondition.substring(11,CityCondition.indexOf(','));
+
+                    CityCondition=CityCondition.substring(CityCondition.indexOf(',')+1);
+
+                    String deadCount=CityCondition.substring(10,CityCondition.indexOf(','));
+
+
+                    System.out.println("省份:"+ProvinceName+"\t 城市:"+CityName+"\t 今日确诊:"+TodayConfirmed+"\t 总共确诊:"
+                            +TotallyConfirmed+"\t 疑似病例:"+suspectedCount+"\t 治愈病列:"+CuredCount+
+                            "\t 死亡病例:"+deadCount);
+
+                    //System.out.println("省份"+ProvinceName+" "+CitySituationMatcher.group(0));
                 }
-            }
+
 
             //System.out.println(ChinaMatcher.group(0));
 
 
-            System.out.println("\n\n");
+            System.out.println("\n");
         }
 
 
